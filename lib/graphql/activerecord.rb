049344e71305f834b93d9feb2ea18fa050464c46
Ryan Foster <theorygeek@gmail.com>
2016-02-24 13:54:21 -0600

    Fixing auto-reload bug

--- lib/graphql/activerecord.rb
+++ lib/graphql/activerecord.rb
@@ -1,8 +1,8 @@
 require 'graphql'
 
+require 'graphql/models/monkey_patches/base_type'
 require 'graphql/models/monkey_patches/graphql_relay_global_node_identification'
 require 'graphql/models/monkey_patches/graphql_query_context'
-require 'graphql/models/monkey_patches/base_type'
 
 # Helpers
 require 'graphql/models/definer'
@@ -11,8 +11,8 @@ require 'graphql/models/definer'
 require 'graphql/models/identification'
 require 'graphql/models/identification/attribute_type_definition'
 require 'graphql/models/identification/attribute_types'
-require 'graphql/models/identification/model_types'
 require 'graphql/models/identification/node_identification'
+require 'graphql/models/identification/model_types'
 require 'graphql/models/proxy_block'
 require 'graphql/models/definition_helpers'
 require 'graphql/models/definition_helpers/associations'
