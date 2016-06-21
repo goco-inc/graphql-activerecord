cc6f7fb041d65425635093b33c26590f335bba9b
theorygeek <theorygeek@gmail.com>
2016-06-21 10:24:40 -0500

    Use a transaction when saving models

--- lib/graphql/models/mutator.rb
+++ lib/graphql/models/mutator.rb
@@ -32,17 +32,20 @@ module GraphQL::Models
 
     def save!
       fail StandardError.new("Need to call apply_changes before #{__method__}") unless @all_changes
-      changed_models.each do |model|
-        next if model.destroyed?
+      
+      ActiveRecord::Base.transaction do
+        changed_models.each do |model|
+          next if model.destroyed?
 
-        if model.marked_for_destruction?
-          model.destroy
-        else
-          model.save!
+          if model.marked_for_destruction?
+            model.destroy
+          else
+            model.save!
+          end
         end
-      end
 
-      changed_models.reject(&:destroyed?).each(&:reload)
+        changed_models.reject(&:destroyed?).each(&:reload)
+      end
     end
   end
 
--- lib/graphql/models/version.rb
+++ lib/graphql/models/version.rb
@@ -1,5 +1,5 @@
 module GraphQL
   module Models
-    VERSION = "0.6.2"
+    VERSION = "0.6.3"
   end
 end
