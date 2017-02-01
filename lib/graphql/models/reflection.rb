443abe5121d24753b92e9a86081d2265e577196f
theorygeek <theorygeek@gmail.com>
2017-02-01 17:46:17 -0600

    Check DB nullability and handle postgres arrays

--- lib/graphql/models/reflection.rb
+++ lib/graphql/models/reflection.rb
@@ -14,7 +14,10 @@ module GraphQL::Models
       end
 
       # Determines if the attribute (or association) is required by examining presence validators
+      # and the nullability of the column in the database
       def is_required(model_class, attr_or_assoc)
+        return true if model_class.columns_hash[attr_or_assoc.to_s]&.null == false
+
         model_class.validators_on(attr_or_assoc)
           .select { |v| v.is_a?(ActiveModel::Validations::PresenceValidator) }
           .reject { |v| v.options.include?(:if) || v.options.include?(:unless) }
@@ -29,19 +32,24 @@ module GraphQL::Models
           DatabaseTypes::TypeStruct.new(type, type)
         else
           # See if it's a registered scalar type
-          active_record_type = model_class.type_for_attribute(attribute.to_s).type
+          active_record_type = model_class.type_for_attribute(attribute.to_s)
 
-          if active_record_type.nil?
+          if active_record_type.type.nil?
             fail ArgumentError, "The type for attribute #{attribute} wasn't found on #{model_class.name}"
           end
 
-          result = DatabaseTypes.registered_type(active_record_type)
+          result = DatabaseTypes.registered_type(active_record_type.type)
 
           if !result
             fail RuntimeError, "The type #{active_record_type} is not registered with DatabaseTypes (attribute #{attribute} on #{model_class.name})"
           end
 
-          result
+          # Arrays: Rails doesn't have a generalized way to detect arrays, so we use this method to do it:
+          if active_record_type.class.name.ends_with?('Array')
+            DatabaseTypes::TypeStruct.new(result.input.to_list_type, result.output.to_list_type)
+          else
+            result
+          end
         end
       end
     end
