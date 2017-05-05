7a79cec152a65f020a952a32bcbb2f5b2d36010c
theorygeek <theorygeek@gmail.com>
2017-05-05 12:37:42 -0500

    Making connections work with GraphQL 1.5.10

---
+++ lib/graphql/models/instrumentation.rb
@@ -0,0 +1,25 @@
+class GraphQL::Models::Instrumentation
+  # @param skip_nil_models If true, field resolvers (in proxy_to or backed_by_model blocks) will not be invoked if the model is nil.
+  def initialize(skip_nil_models = true)
+    @skip_nil_models = skip_nil_models
+  end
+
+  def instrument(type, field)
+    field_info = GraphQL::Models.field_info(type, field.name)
+    return field unless field_info
+
+    old_resolver = field.resolve_proc
+    
+    new_resolver = -> (object, args, ctx) {
+      base_model = field_info.object_to_base_model.call(object)
+      GraphQL::Models.load_association(base_model, field_info.path, ctx).then do |model|
+        next nil if model.nil? && @skip_nil_models
+        old_resolver.call(model, args, ctx)
+      end
+    }
+
+    field.redefine do
+      resolve(new_resolver)
+    end
+  end
+end
