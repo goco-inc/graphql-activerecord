62a87ec40e63b3687c667c1c5ec502c480889ffc
Nate Strandberg <nate@juliabalfour.com>
2017-06-01 09:13:16 -0400

    Removed code from `get_graphql_type` that expected GraphQL types to be namespaced (should not have been in this commit!)

--- lib/graphql/activerecord.rb
+++ lib/graphql/activerecord.rb
@@ -88,7 +88,7 @@ module GraphQL
       if model_to_graphql_type
         model_to_graphql_type[model_class]
       else
-        "Types::#{model_class.name}Type".safe_constantize
+        "#{model_class.name}Type".safe_constantize
       end
     end
 
