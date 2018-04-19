557256820d9bfc2214793a52fd61fb97bc1747f6
Ryan Foster <theorygeek@gmail.com>
2018-04-18 21:21:55 -0500

    Backed by model can return promise

--- lib/graphql/models/instrumentation.rb
+++ lib/graphql/models/instrumentation.rb
@@ -12,10 +12,11 @@ class GraphQL::Models::Instrumentation
     old_resolver = field.resolve_proc
 
     new_resolver = -> (object, args, ctx) {
-      base_model = field_info.object_to_base_model.call(object)
-      GraphQL::Models.load_association(base_model, field_info.path, ctx).then do |model|
-        next nil if model.nil? && @skip_nil_models
-        old_resolver.call(model, args, ctx)
+      Promise.resolve(field_info.object_to_base_model.call(object)).then do |base_model|
+        GraphQL::Models.load_association(base_model, field_info.path, ctx).then do |model|
+          next nil if model.nil? && @skip_nil_models
+          old_resolver.call(model, args, ctx)
+        end
       end
     }
 
